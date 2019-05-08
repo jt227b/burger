d9dcbc2affec87d3a2c17b22b7da26f02f3cfad2
Jose Torres <jt227b@gmail.com>
2019-05-07 20:15:19 -0500

    Cotinued Config Folder with orm's

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,31 @@
-// Dependencies //
+// Dependencies // // Dependencies // // Dependencies //
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else{
 
 // Remeber to Erase Password before commiting // // Remeber to Erase Password before commiting //
-    password: "",
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
+// Remeber to Erase Password before commiting // // Remeber to Erase Password before commiting //
 
-    database: "burgers_db"
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.error("Connection error: " + err.stack);
-        return;
+connection.connect(function(err){
+    if(err){
+        console.error("error connecting: " + err.stack);
+        return
     }
-    console.log("Connected as id " + connection.threadId);
+    console.log("connected as id: " + connection.threadId);
 });
+
 
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,83 @@
-// Dependencies //
+// Dependencies // // Import MySQL connection // // Dependencies // // Import MySQL connection // 
 var connection = require("./connection");
 
-var orm = {
-    selectAll();
+function printQuestionMarks(num) {
+    var arr = [];
 
-    insertOne();
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
 
-    updateOne();
-
+    return arr.toString();
 }
 
+// Helper function to convert object key/value pairs to SQL syntax //
+function objToSql(ob) {
+    var arr = [];
 
-module.exports = orm;
+ 
+    for (var key in ob) {
+        var value = ob[key];
+       
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
+// Object for all our SQL statement functions //
+var orm = {
+    selectAll: function(tableInput, cb) {
+    
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    insertOne: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table + " (" + cols.toString() + ") VALUES (" + printQuestionMarks(vals.length) +  ");"
+     
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    
+    updateOne: function(table, colValObj, id, cb) {
+      var queryString = "UPDATE " + table + " SET " + objToSql(colValObj) + " WHERE " + id
+  
+     
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    delete: function(table, id, cb) {
+      var queryString = "DELETE FROM " + table + " WHERE " + id
+  
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    }
+};
+  
+ 
+  module.exports = orm;
+  
