d1c63c81dca6f81f466c42bb922b865d881a4636
Jose Torres <jt227b@gmail.com>
2019-05-07 20:58:32 -0500

    Added Database codes for MySql workbench

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,16 @@
--- Drops Database
+### Schema
 DROP DATABASE IF EXISTS burgers_db;
-
--- Create burgers_db 
 CREATE DATABASE burgers_db;
-
 USE burgers_db;
 
--- Create table
-CREATE TABLE burgers (
-   
-    id INT NOT NULL AUTO_INCREMENT,
-    
-    burger_name VARCHAR(255) NOT NULL,
-   
-    devoured BOOL DEFAULT false,
-    
-    PRIMARY KEY (id)
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(250) NOT NULL ,
+	devoured BOOLEAN DEFAULT false,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now(),
+	PRIMARY KEY (id)
 );
+
+SELECT * FROM burgers;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,4 @@
--- Queries table for burgers
-INSERT INTO burgers (burger_name, devoured)
-
 -- Table entries
-VALUES ("Happy Meal Cheese Burger", FALSE),
-  ("Whopper Jr", FALSE),
-  ("Baconator", FALSE);
+INSERT INTO burgers (burger_name) VALUES ('Happy Meal Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whopper Jr');
+INSERT INTO burgers (burger_name) VALUES ('Whataburger Jr w. Cheese');
