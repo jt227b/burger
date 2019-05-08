80420e0e9ca826b0ab8c4f5b06e869d6b2392e58
Jose Torres <jt227b@gmail.com>
2019-05-07 20:39:17 -0500

    Updated JS on Controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,55 @@
 var express = require("express");
-var burger = require("../models/burger");
 
 var router = express.Router();
 
+var burger = require("../models/burger");
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/burgers", function (req, res) {
+    burger.insert(["burger_name"], [req.body.burger_name], function (result) {
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put("/burgers/:id", function (req, res) {
+    var id = "id = " + req.params.id;
+
+    burger.update({
+        devoured: req.body.devoured
+    },
+        id, function (result) {
+            if (result.changedRows == 0) {
+
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+});
+
+router.delete("/burgers/:id", function (req, res) {
+    var id = "id = " + req.params.id;
+
+    burger.delete(id, function (result) {
+        if (result.affectedRows == 0) {
+
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 module.exports = router;
