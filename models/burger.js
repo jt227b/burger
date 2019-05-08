d757b06d58e685cf3c7942a75a728353ac2c3b8f
Jose Torres <jt227b@gmail.com>
2019-05-07 21:10:11 -0500

    Updating burger.js linked to config/orm

--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,27 @@
 var orm = require("../config/orm");
 
 var burger = {
-    selectAll();
-
-    insertOne();
-
-    updateOne();
-
-}
-
-
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+ 
+  insert: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(colValObj, id, cb) {
+    orm.updateOne("burgers", colValObj, id, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(id, cb) {
+    orm.delete("burgers", id, function(res) {
+      cb(res);
+    });
+  }
+};
 
 module.exports = burger;
